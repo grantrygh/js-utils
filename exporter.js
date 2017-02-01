6f6315a0a10047ffac0528db4087156b3e657557
Tushar Singh <tusharsingh@gmx.com>
2017-02-01 19:58:24 +0530

    createChainableTypeChecker

--- exporter.js
+++ exporter.js
@@ -2,21 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const watch = require('node-watch');
 
+const shouldWatch = process.argv[2];
+
 // Write util exports
 const writeExports = () => {
     const utils = fs.readdirSync(path.resolve('lib'));
-    let content = 'module.exports = {';
+    let content = '';
 
     utils.forEach(util => {
         const component = util.split('.js')[0];
-        content += '\n    ' + component + ': require(\'./lib/'+ component +'.js\').default,';
+        content += 'exports.' + component + ' = require(\'./lib/'+ component +'.js\').default;\n';
     });
 
-    content += '\n};\n';
+    // content += '\n};\n';
 
     fs.writeFileSync('index.js', content);
-    console.log('updated');
+    console.log('Exporter: updated exports.');
 }
 
-watch('./lib', writeExports);
 writeExports();
+
+if (shouldWatch) watch('./lib', writeExports);
