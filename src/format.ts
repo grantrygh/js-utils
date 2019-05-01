2e3db1bc9cb3f480f3e12f8175bafa8c32719b58
Tushar Singh <tusharsingh@gmx.com>
2019-05-01 16:41:58 +0530

    format util

---
+++ src/format.ts
@@ -0,0 +1,12 @@
+import { ordinalSuffix } from './ordinalSuffix';
+
+type formatType = 'number' | 'ordinal';
+
+export function format(value: number, type: formatType = 'number') {
+    switch (type) {
+        case 'number':
+            return Intl.NumberFormat('en').format(value);
+        case 'ordinal':
+            return ordinalSuffix(value);
+    }
+}
